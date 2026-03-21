4c8d39a1f3ba3191d3c528111eccf37f09e6945e
Carlos Sandoval <csandovalc@unal.edu.co>
2026-03-21 18:48:40 -0500

    feat(token): add base headers for token recognition
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

---
+++ include/charclass.h
@@ -0,0 +1,39 @@
+#pragma once
+
+typedef enum {
+    CHAR_LETTER,
+    CHAR_DIGIT,
+    CHAR_UNDERSCORE,
+    CHAR_PLUS,
+    CHAR_MINUS,
+    CHAR_STAR,
+    CHAR_SLASH,
+    CHAR_PERCENT,
+    CHAR_EQUAL,
+    CHAR_BANG,          // !
+    CHAR_LESS,
+    CHAR_GREATER,
+    CHAR_AMPERSAND,     // &
+    CHAR_PIPE,          // |
+    CHAR_QUESTION,
+    CHAR_DOT,
+    CHAR_CARET,         // ^
+    CHAR_TILDE,         // ~
+    CHAR_APOSTROPHE,    // '
+    CHAR_QUOTE,         // "
+    CHAR_GRAVE_ACCENT,  // `
+    CHAR_COMMA,
+    CHAR_SEMICOLON,
+    CHAR_COLON,
+    CHAR_LBRACE,        // {
+    CHAR_RBRACE,        // }
+    CHAR_LBRACKET,      // [
+    CHAR_RBRACKET,      // ]
+    CHAR_LPAREN,        // (
+    CHAR_RPAREN,        // )
+    CHAR_WHITESPACE,
+    CHAR_EOF,
+    CHAR_OTHER
+} CharClass;
+
+CharClass classify_char(char c);
---
+++ include/dfa.h
@@ -0,0 +1,42 @@
+#pragma once
+
+typedef enum {
+    STATE_START,
+
+    // Basic States
+    STATE_IDENTIFIER,
+    STATE_NUMBER,
+    STATE_DOT_NUMER,    // This supports float numbers
+
+    // String States
+    STATE_STRING_DOUBLE,            // "..."
+    STATE_STRING_DOUBLE_ESCAPE,
+    STATE_STRING_SINGLE,            // '...'
+    STATE_STRING_SINGLE_ESCAPE,
+    STATE_STRING_BACKTICK,          // `...`
+    STATE_STRING_BACKTICK_ESCAPE,
+
+    // Ambiguous prefix (needs lookahead)
+    STATE_PLUS,
+    STATE_MINUS,
+    STATE_STAR,         // *
+    STATE_SLASH,
+    STATE_PERCENT,
+    STATE_EQUAL,
+    STATE_BANG,         // !
+    STATE_LESS,
+    STATE_GREATER,
+    STATE_AMPERSAND,    // &
+    STATE_PIPE,         // |
+    STATE_QUESTION,
+    STATE_DOT,          // This state requires lookahead of 2 chars!
+
+    // Longer Ambiguous Variants
+    STATE_STAR_STAR,    // **
+    STATE_EQUAL_EQUAL,  // ==
+    STATE_BANG_EQUAL,   // !=
+
+    // Final States
+    STATE_DONE,
+    STATE_ERROR,
+} LexerState;
