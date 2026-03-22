c90f1d3272b3e40e8200549b90b55b2d4d5c630e
Carlos Sandoval <csandovalc@unal.edu.co>
2026-03-21 20:19:33 -0500

    feat(buffer): implement a basic I/O buffer
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

---
+++ include/buffer.h
@@ -0,0 +1,20 @@
+#ifndef ESJS_CUSTOM_LEXER_BUFFER_H
+#define ESJS_CUSTOM_LEXER_BUFFER_H
+
+#include <stdio.h>
+
+typedef struct {
+    FILE *input;        // Data origin
+
+    char *data;         // Stored data
+    size_t length;      // Number of chars loaded into buffer
+    size_t capacity;    // Allocated size
+    int eof;            // Boolean that indicate if the EOF is reached
+} Buffer;
+
+void buffer_init(Buffer *buf, FILE *input);
+void buffer_destroy(Buffer *buf);
+int buffer_get(Buffer *buf, size_t pos);
+int buffer_is_eof(const Buffer *buf);
+
+#endif //ESJS_CUSTOM_LEXER_BUFFER_H
--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include "../include/buffer.h"
 
-int main() {
-  return 0;
+int main(void) {
+    Buffer buf;
+    int c;
+    size_t i = 0;
+
+    buffer_init(&buf, stdin);
+    while ((c = buffer_get(&buf, i)) != EOF) {
+        printf("buf[%zu] = '%c'\n", i, c);
+        i++;
+    }
+
+    buffer_destroy(&buf);
+    return 0;
 }
