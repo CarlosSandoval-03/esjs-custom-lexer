6663c92d6972e642e425972e1f1bf2fab1fdbfc1
Carlos Sandoval <csandovalc@unal.edu.co>
2026-03-21 19:43:49 -0500

    feat(charclass): implementation of the classification
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- include/charclass.h
+++ include/charclass.h
@@ -5,38 +5,43 @@ typedef enum {
     CHAR_LETTER,
     CHAR_DIGIT,
     CHAR_UNDERSCORE,
-    CHAR_PLUS,
-    CHAR_MINUS,
-    CHAR_STAR,
-    CHAR_SLASH,
-    CHAR_PERCENT,
-    CHAR_EQUAL,
-    CHAR_BANG,          // !
-    CHAR_LESS,
-    CHAR_GREATER,
-    CHAR_AMPERSAND,     // &
-    CHAR_PIPE,          // |
-    CHAR_QUESTION,
-    CHAR_DOT,
-    CHAR_CARET,         // ^
-    CHAR_TILDE,         // ~
-    CHAR_APOSTROPHE,    // '
-    CHAR_QUOTE,         // "
-    CHAR_GRAVE_ACCENT,  // `
-    CHAR_COMMA,
-    CHAR_SEMICOLON,
-    CHAR_COLON,
-    CHAR_LBRACE,        // {
-    CHAR_RBRACE,        // }
-    CHAR_LBRACKET,      // [
-    CHAR_RBRACKET,      // ]
-    CHAR_LPAREN,        // (
-    CHAR_RPAREN,        // )
     CHAR_WHITESPACE,
+    CHAR_NEWLINE,
+
+    CHAR_PLUS,        // +
+    CHAR_MINUS,       // -
+    CHAR_STAR,        // *
+    CHAR_SLASH,       // /
+    CHAR_PERCENT,     // %
+    CHAR_EQUAL,       // =
+    CHAR_BANG,        // !
+    CHAR_LESS,        // <
+    CHAR_GREATER,     // >
+    CHAR_AMPERSAND,   // &
+    CHAR_PIPE,        // |
+    CHAR_CARET,       // ^
+    CHAR_TILDE,       // ~
+    CHAR_QUESTION,    // ?
+    CHAR_DOT,         // .
+
+    CHAR_COMMA,       // ,
+    CHAR_SEMICOLON,   // ;
+    CHAR_COLON,       // :
+    CHAR_LBRACE,      // {
+    CHAR_RBRACE,      // }
+    CHAR_LBRACKET,    // [
+    CHAR_RBRACKET,    // ]
+    CHAR_LPAREN,      // (
+    CHAR_RPAREN,      // )
+
+    CHAR_DOUBLE_QUOTE, // "
+    CHAR_SINGLE_QUOTE, // '
+    CHAR_BACKTICK,     // `
+
     CHAR_EOF,
     CHAR_OTHER
 } CharClass;
 
-CharClass classify_char(char c);
+CharClass classify_char(int c);
 
 #endif // ESJS_CUSTOM_LEXER_BUFFER_H
---
+++ src/charclass.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "../include/charclass.h"
+
+
+CharClass classify_char(const int c) {
+    if (c == EOF) return CHAR_EOF;
+    if (c == '\n') return CHAR_NEWLINE;
+    if (c == ' ' || c == '\t' || c == '\r' || c == '\f' || c == '\v') return CHAR_WHITESPACE;
+
+    if (isalpha((unsigned char)c)) return CHAR_LETTER;
+    if (isdigit((unsigned char)c)) return CHAR_DIGIT;
+
+    switch (c) {
+        case '_': return CHAR_UNDERSCORE;
+        case '+': return CHAR_PLUS;
+        case '-': return CHAR_MINUS;
+        case '*': return CHAR_STAR;
+        case '/': return CHAR_SLASH;
+        case '%': return CHAR_PERCENT;
+        case '=': return CHAR_EQUAL;
+        case '!': return CHAR_BANG;
+        case '<': return CHAR_LESS;
+        case '>': return CHAR_GREATER;
+        case '&': return CHAR_AMPERSAND;
+        case '|': return CHAR_PIPE;
+        case '^': return CHAR_CARET;
+        case '~': return CHAR_TILDE;
+        case '?': return CHAR_QUESTION;
+        case '.': return CHAR_DOT;
+
+        case ',': return CHAR_COMMA;
+        case ';': return CHAR_SEMICOLON;
+        case ':': return CHAR_COLON;
+        case '{': return CHAR_LBRACE;
+        case '}': return CHAR_RBRACE;
+        case '[': return CHAR_LBRACKET;
+        case ']': return CHAR_RBRACKET;
+        case '(': return CHAR_LPAREN;
+        case ')': return CHAR_RPAREN;
+
+        case '"': return CHAR_DOUBLE_QUOTE;
+        case '\'': return CHAR_SINGLE_QUOTE;
+        case '`': return CHAR_BACKTICK;
+
+        default:
+            return CHAR_OTHER;
+    }
+}
